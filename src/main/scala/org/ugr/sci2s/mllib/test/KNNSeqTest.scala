4263e9f3e830d260e216853aefdc644c3e480d7d
Sergio R <srg00017@gmail.com>
2016-03-21 13:48:16 +0100

    Added some little changes in the test files

--- src/main/scala/org/ugr/sci2s/mllib/test/KNNSeqTest.scala
+++ src/main/scala/org/ugr/sci2s/mllib/test/KNNSeqTest.scala
@@ -15,13 +15,14 @@ object KNNSeqTest {
     
     // Create a local StreamingContext with two working thread and batch interval of 1 second.
     // The master requires 2 cores to prevent from a starvation scenario.
-
+    val k = 5
     val points = scala.util.Random.shuffle(scala.io.Source.fromFile("/home/sramirez/datasets/poker-5-fold/streaming/poker-small.tra/part-asd.dat")
         .getLines().toSeq.map(LabeledPoint.parse))
     val tree = new MTreeLP()
     points.foreach (p => tree.insert(p.features.toArray.map(_.toFloat), p.label.toFloat))    
+    val result = points.map( p => tree.kNNQuery(k, p.features.toArray.map(_.toFloat)).map(t => (t.getDistance.toFloat, t.getPoint.getLabel)))
     val predict = (p: LabeledPoint) => {
-      val neigh = tree.kNNQuery(1, p.features.toArray.map(_.toFloat)).map(t => (t.getDistance.toFloat, t.getPoint.getLabel))
+      val neigh = tree.kNNQuery(k, p.features.toArray.map(_.toFloat)).map(t => (t.getDistance.toFloat, t.getPoint.getLabel))
       val pred = neigh.map(_._2).groupBy(identity).maxBy(_._2.size)._1
       (p.label, pred)
     }    
