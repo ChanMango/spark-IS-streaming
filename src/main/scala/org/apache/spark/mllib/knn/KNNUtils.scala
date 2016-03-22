d61c28d2cf2b7afb6acd951f581481364d4f62a7
Sergio R <srg00017@gmail.com>
2016-03-22 14:30:49 +0100

    KNNutils now contains the definition of VectorWithNorm

--- src/main/scala/org/apache/spark/mllib/knn/KNNUtils.scala
+++ src/main/scala/org/apache/spark/mllib/knn/KNNUtils.scala
@@ -2,6 +2,8 @@ package org.apache.spark.mllib.knn
 
 import org.apache.spark.mllib.linalg.Vector
 import org.apache.spark.mllib.util.MLUtils
+import breeze.linalg.{DenseVector => BDV, Vector => BV}
+import org.apache.spark.mllib.linalg.Vectors
 
 object KNNUtils {
   def fastSquaredDistance(
@@ -11,4 +13,19 @@ object KNNUtils {
                            norm2: Double,
                            precision: Double = 1e-6): Double =
     MLUtils.fastSquaredDistance(v1, norm1, v2, norm2, precision)
+}
+
+/**
+  * VectorWithNorm can use more efficient algorithm to calculate distance
+  */
+case class VectorWithNorm(vector: Vector, norm: Double) {
+  def this(vector: Vector) = this(vector, Vectors.norm(vector, 2))
+
+  def this(vector: BV[Double]) = this(Vectors.fromBreeze(vector))
+
+  def fastSquaredDistance(v: VectorWithNorm): Double = {
+    KNNUtils.fastSquaredDistance(vector, norm, v.vector, v.norm)
+  }
+
+  def fastDistance(v: VectorWithNorm): Double = math.sqrt(fastSquaredDistance(v))
 }
