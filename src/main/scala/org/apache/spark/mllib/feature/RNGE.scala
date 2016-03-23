64d240be661e8f793afe945dd315e27a010ef22a
Sergio R <srg00017@gmail.com>
2016-03-23 13:40:36 +0100

    Fixed a bug with RNGE 2nd order.

--- src/main/scala/org/apache/spark/mllib/feature/RNGE.scala
+++ src/main/scala/org/apache/spark/mllib/feature/RNGE.scala
@@ -6,6 +6,7 @@ import org.apache.spark.mllib.feature.{StreamingDistributedKNN => SDK}
 import org.apache.spark.mllib.knn.VectorWithNorm
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.HashMap
+import org.apache.spark.storage.StorageLevel
 
 /**
  * @author sramirez
@@ -43,11 +44,10 @@ object RNGE {
             votes(i) += lpnorms(j)._1 -> (votes(i).getOrElse(lpnorms(j)._1, 0) + 1)
         }
       }
-      val preds = votes.map(_.maxBy(_._2)._1)
+      var preds = votes.map(_.maxBy(_._2)._1)
       
       if(secondOrder) {        
         /* 2nd order voting */
-        //val svotes = votes.map(_.clone)
         for(i <- 0 until lpnorms.length if preds(i) != lpnorms(i)._1){
           for(j <- 0 until lpnorms.length){
             if(graph(i)(j) && i != j && lpnorms(i)._1 == lpnorms(j)._1){
@@ -56,15 +56,17 @@ object RNGE {
             }
           }
         }
+        preds = votes.map(_.maxBy(_._2)._1)
       }
       
       /* Decide whether to add the new example and to remove old noisy edges */
       val noisy = (0 until preds.length).map(i => preds(i) != lpnorms(i)._1)
-      val toAdd = if(!noisy(0)) point else null // Accepted the insertion of new example
+      val toAdd = if(!noisy(0)) point else null // Accepted the insertion of new example (first)
       val toRemove = neigs.zipWithIndex.filter(t => noisy(t._2 + 1)).map(_._1) // Remove noisy oldies
       (toAdd, toRemove)
       
-    }    
+    }.persist(StorageLevel.MEMORY_AND_DISK)
+    
     (edited.keys.filter(_ != null), edited.flatMap(_._2))
   }
 }
