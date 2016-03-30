f3156f30d0bf40a5088c54694f8569361d65d6f3
Sergio R <srg00017@gmail.com>
2016-03-30 17:54:13 +0200

    Added time measurement to KNN sequential.

--- src/main/scala/org/ugr/sci2s/mllib/test/KNNSeqTest2.scala
+++ src/main/scala/org/ugr/sci2s/mllib/test/KNNSeqTest2.scala
@@ -18,16 +18,21 @@ object KNNSeqTest2 {
     
     // Create a local StreamingContext with two working thread and batch interval of 1 second.
     // The master requires 2 cores to prevent from a starvation scenario.
-    val k = 1
-    val points = scala.util.Random.shuffle(scala.io.Source.fromFile("/home/sramirez/datasets/poker-5-fold/streaming/poker-small.tra/part-asd.dat")
-        .getLines().toSeq.map(LabeledPoint.parse))
+    val k = 10
+    /*val points = scala.util.Random.shuffle(scala.io.Source.fromFile("/home/sramirez/datasets/poker-5-fold/streaming/part-asd.dat")
+        .getLines().toSeq.map(LabeledPoint.parse))*/
+    val points = scala.io.Source.fromFile("/home/sramirez/datasets/poker-5-fold/streaming/poker-10K.dat").getLines().toSeq.map(LabeledPoint.parse)
     val tree = new MTreeWrapper(points.map(lp => new DataLP(lp.features.toArray.map(_.toFloat), lp.label.toFloat)).toArray)
     val predict = (p: LabeledPoint) => {
       val neigh = tree.kNNQuery(k, p.features.toArray.map(_.toFloat)).map(t => (t.distance, t.data.asInstanceOf[DataLP]))
       val pred = neigh.map(_._2.getLabel).groupBy(identity).maxBy(_._2.size)._1
-      (p.label, pred)
+      (p.label.toFloat, pred)
     }    
+    val startTime = System.currentTimeMillis()    
     println("Number of matches: " + points.map(predict).filter{ case (l,p) => l == p}.length)
+    val estimatedTime = (System.currentTimeMillis() - startTime) / 1000f
+    println("Estimated Time: " + estimatedTime)
+    //println("Number of matches: " + preds.filter{ case (l,p) => l == p}.length)
   }
 
 }
