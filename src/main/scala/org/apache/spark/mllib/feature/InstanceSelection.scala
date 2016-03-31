fce7cd0826b4fff937f8e5b25bc1355ccdd8ee07
Sergio <srg00017@gmail.com>
2016-03-31 12:45:13 +0200

    Renamed file to include the new condensation method: CNN.

--- src/main/scala/org/apache/spark/mllib/feature/InstanceSelection.scala
+++ src/main/scala/org/apache/spark/mllib/feature/InstanceSelection.scala
@@ -3,12 +3,10 @@ package org.apache.spark.mllib.feature
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.feature.{StreamingDistributedKNN => SDK}
 import org.apache.spark.mllib.knn.VectorWithNorm
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.HashMap
 import org.apache.spark.storage.StorageLevel
-
 import org.apache.spark.mllib.feature.StreamingDistributedKNN._
 
 /**
@@ -17,11 +15,11 @@ import org.apache.spark.mllib.feature.StreamingDistributedKNN._
 
 
 
-object RNGE {
+object InstanceSelection {
   
   case class TreeLPNorm(point: TreeLP, norm: VectorWithNorm)
   
-  def edition(neighbors: RDD[(TreeLP, Array[TreeLP])], secondOrder: Boolean = false) = {
+  def RNGE(neighbors: RDD[(TreeLP, Array[TreeLP])], secondOrder: Boolean = false) = {
     
     neighbors.flatMap{ case (p, neigs) => 
       
@@ -73,5 +71,56 @@ object RNGE {
       val toRemove = neigs.zipWithIndex.filter(t => noisy(t._2 + 1)).map{p => p._1.action = REMOVE; p._1} // Remove noisy oldies
       if(!noisy(0)) p +: toRemove else toRemove      
     }
-  }
+  }  
+  
+  def CNN(neighbors: RDD[(TreeLP, Array[TreeLP])], k: Int, seed: Long) = {
+    
+    neighbors.flatMap{ case (p, neigs) => 
+      
+      val lpnorms = new TreeLPNorm(new TreeLP(p.point, p.itree, INSERT), new VectorWithNorm(p.point.features)) +:
+        neigs.map(q => new TreeLPNorm(new TreeLP(q.point, q.itree, REMOVE), new VectorWithNorm(q.point.features)))
+        
+      var S = new ArrayBuffer[Int]
+      val rand = new scala.util.Random(seed)
+      
+      /*Inserting one element per class*/
+      val classes = lpnorms.map(_.point.point.label).distinct
+      for(c <- classes) {
+        var pos = rand.nextInt(lpnorms.length); var cont = 0
+        while (lpnorms(pos).point.point.label != c && cont < lpnorms.length) {
+			    pos = (pos + 1) % lpnorms.length
+			    cont += 1
+		    }        
+        if (cont < lpnorms.length) {
+			    S += pos
+		    }
+      }
+
+		  /*Algorithm body. We resort randomly the instances of T and compare with the rest of S.
+		 	If an instance doesn't classified correctly, it is inserted in S*/
+      var continue = false
+  		do {
+  		  continue = false
+  		  val shuffled = scala.util.Random.shuffle(S.toSeq)
+  		  S = S.sorted
+  		  for(i <- 0 until lpnorms.length){  		    
+  		    val pos = shuffled(i)
+  		    val unselected = java.util.Arrays.binarySearch(S.asInstanceOf[Array[AnyRef]], pos) < 0
+  		    if(unselected) {  		      
+  		      val knn = S.map(idx => lpnorms(pos).norm.fastDistance(lpnorms(idx).norm) -> lpnorms(idx))
+  		        .sortBy(-_._1).take(k)
+  		      val pred = knn.map(_._2.point.point.label).groupBy(identity).maxBy(_._2.size)._1
+  		      if(pred != lpnorms(pos).point.point.label){
+  		        continue = true
+  		        S += pos
+  		        S = S.sorted
+  		      }
+  		    }		      
+  		  }
+  		} while (continue)
+  		S
+    }
+  } 
+  
+  
 }
