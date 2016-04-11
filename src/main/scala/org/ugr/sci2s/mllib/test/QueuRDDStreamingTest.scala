62a5a735978b0129f493bb3a279e03de65639cfb
Sergio R <srg00017@gmail.com>
2016-04-11 13:45:35 +0200

    Added some extra parameters to the main file. POM file modified to compile well in cluster environments.

--- src/main/scala/org/ugr/sci2s/mllib/test/QueuRDDStreamingTest.scala
+++ src/main/scala/org/ugr/sci2s/mllib/test/QueuRDDStreamingTest.scala
@@ -14,7 +14,7 @@ object QueuRDDStreamingTest {
     // Create a local StreamingContext with two working thread and batch interval of 1 second.
     // The master requires 2 cores to prevent from a starvation scenario.
 
-    val conf = new SparkConf().setMaster("local[4]").setAppName("MLStreamingTest")
+    val conf = new SparkConf().setAppName("MLStreamingTest")
     val ssc = new StreamingContext(conf, Seconds(2))
     val sc = ssc.sparkContext
     
@@ -28,29 +28,35 @@ object QueuRDDStreamingTest {
         }
     }).toMap  
     
+    //val input = params.getOrElse("input", "/home/sramirez/datasets/poker-5-fold/poker-5-1tra.data") 
+    //val header = params.getOrElse("header", "/home/sramirez/datasets/poker-5-fold/poker.header") 
     
-    val input = params.getOrElse("input", "/user/spark/datasets/kddcup_full_normal_versus_DOS-5-fold/kddcup_full_normal_versus_DOS-5-1tra.data") 
-    val header = params.getOrElse("header", "/user/spark/datasets/kddcup_full_normal_versus_DOS-5-fold/kddcup_full_normal_versus_DOS.info") 
+    val input = params.getOrElse("input", "/home/sramirez/datasets/kddcup_10_normal_versus_DOS-5-fold/kddcup_10_normal_versus_DOS-5-1tra.data") 
+    val header = params.getOrElse("header", "/home/sramirez/datasets/kddcup_10_normal_versus_DOS-5-fold/kddcup_10_normal_versus_DOS.header") 
+    val npart = params.getOrElse("npart", "4").toInt     
+    val k = params.getOrElse("k", "1").toInt
+    val rate = params.getOrElse("rate", "2500").toInt
+    val seed = params.getOrElse("seed", "237597430").toLong
+    val ntrees = params.getOrElse("ntress", "4").toInt
+    
+    // Read file
     val typeConversion = KeelParser.parseHeaderFile(sc, header) 
     val bcTypeConv = sc.broadcast(typeConversion)
-    val lines = sc.textFile(input: String)
-    lines.map(line => KeelParser.parseLabeledPoint(bcTypeConv.value, line))   
+    val inputRDD = sc.textFile(input: String).map(line => KeelParser.parseLabeledPoint(bcTypeConv.value, line))
+      .repartition(npart)
+      .cache
     
-    val k = params.getOrElse("k", "1").toInt
-    val rate = params.getOrElse("rate", "1000000").toInt
-    val seed = params.getOrElse("seed", "237597430").toLong
-    
-    val inputRDD = sc.textFile(input).map(LabeledPoint.parse).cache
+    // Transform simple RDD into a QueuRDD for streaming
+    //val inputRDD = sc.textFile(input).repartition().map(LabeledPoint.parse).cache
     val size = inputRDD.count()
     val chunkPerc = rate.toFloat / size
     val nchunks = math.ceil(1 / chunkPerc).toInt
     println("Number of chunks: " + nchunks)
-    println("Size of chunks: " + chunkPerc)
-    
+    println("Size of chunks: " + chunkPerc)    
     val arrayRDD = inputRDD.randomSplit(Array.fill[Double](nchunks)(chunkPerc), seed)
     val trainingData = ssc.queueStream(scala.collection.mutable.Queue(arrayRDD: _*), oneAtATime = true)
     
-    val model = new StreamingDistributedKNN().setNPartitions(4)
+    val model = new StreamingDistributedKNN().setNPartitions(ntrees)
     val preds = model.predictOnValues(trainingData, k)
         .map{case (label, pred) => if(label == pred) (1, 1)  else (0, 1)}
         .reduce{ case (t1, t2) => (t1._1 + t2._1, t1._2 + t2._2) }
